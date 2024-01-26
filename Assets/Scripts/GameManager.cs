770812078b457d6154dd3cbc9d38ef1d71190ba2
JKadicps <kadic.jakov@gmail.com>
2024-01-26 18:52:58 +0100

    Treci
    playercontroller script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public int timeAfterDeathToRestart;
+    public int timeAfterDeathToRestart = 3;
     private void Awake() {
         instance = this;
     }

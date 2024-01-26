d1eef05838104c382bf9d50a4bd8c7f50aa4ff16
JKadicps <kadic.jakov@gmail.com>
2024-01-26 19:49:57 +0100

    BranchPlayerControllerFinished
    playercontroller script, camera follow player script

--- Assets/Scripts/PlayerScripts/KeyBinds.cs
+++ Assets/Scripts/PlayerScripts/KeyBinds.cs
@@ -7,6 +7,7 @@ public class KeyBinds : MonoBehaviour
     public static KeyBinds instance;
 
     public KeyCode lvlRestartKey = KeyCode.R;
+    public KeyCode sprintKey = KeyCode.LeftShift;
 
     private void Awake() {
         instance = this;

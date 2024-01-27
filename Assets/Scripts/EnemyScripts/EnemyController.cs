afed15cdcb692878ebf9eab8c9e3afdf9c7d0677
JKadicps <kadic.jakov@gmail.com>
2024-01-27 10:48:25 +0100

    Enemy
    enemy controller, patrol scripts

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -9,23 +9,59 @@ public class EnemyController : MonoBehaviour
     
     private Vector3 startingPoint;
     private Transform destinationPoint;
+    [SerializeField] private int minWaitTimeAtPatrolPoint = 3;
+    [SerializeField] private int maxWaitTimeAtPatrolPoint = 10;
+    private float waitTime;
+    private bool atPatrolPoint;
+    private Transform currentPatrolPoint;
 
     private void Awake() {
         agent = GetComponent<NavMeshAgent>();
         startingPoint = transform.position;
+        atPatrolPoint = false;
+    }
+
+    private void Start() {
+        Patrolling();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(atPatrolPoint && waitTime > 0){
+            Debug.Log(waitTime);
+            waitTime = waitTime - Time.deltaTime;
+            if(waitTime <= 0){
+                destinationPoint = null;
+            }
+        }
+        if(destinationPoint == null){
+            Patrolling();
+        }
     }
 
     private void Patrolling(){
         try{
             destinationPoint = PatrolControl.instance.Choosing();
+            agent.destination = destinationPoint.position;
+            waitTime = Random.Range(minWaitTimeAtPatrolPoint, maxWaitTimeAtPatrolPoint);
         }catch{
             Debug.Log("Slobodni patrol point nije naden");
         }
     }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Patrol point"){
+            atPatrolPoint = true;
+            currentPatrolPoint = other.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Patrol point"){
+            atPatrolPoint = false;
+            PatrolControl.instance.LeavingAPatrolPoint(currentPatrolPoint);
+        }
+    }
+
 }
--- Assets/Scripts/EnemyScripts/PatrolControl.cs
+++ Assets/Scripts/EnemyScripts/PatrolControl.cs
@@ -6,7 +6,7 @@ public class PatrolControl : MonoBehaviour
 {   
     public static PatrolControl instance;
     [SerializeField] List<Transform> patrolPoints = new List<Transform>();
-    private List<int> full = new List<int>();  
+    private List<int> enemiesAtSamePatrolPoint = new List<int>();  
     [SerializeField] int maxWorkersAtOnePatrolPoint = 2;
     [SerializeField] int maxWhileLoops = 50;
 
@@ -18,21 +18,31 @@ public class PatrolControl : MonoBehaviour
     public Transform Choosing(){
         int counterForLoops = maxWhileLoops;
         int indexOfPatrolPoint = Random.Range(0, patrolPoints.Count-1);;
-        while(counterForLoops-- > 0 && full[indexOfPatrolPoint] == maxWorkersAtOnePatrolPoint){
+        while(counterForLoops-- > 0 && enemiesAtSamePatrolPoint[indexOfPatrolPoint] == maxWorkersAtOnePatrolPoint){
             indexOfPatrolPoint = Random.Range(0, patrolPoints.Count-1);
         }
         if(counterForLoops == 0){
             return null;
         }
         else{
-            full[indexOfPatrolPoint]++;
+            enemiesAtSamePatrolPoint[indexOfPatrolPoint]++;
+            Debug.Log("New patrol point destination" + (indexOfPatrolPoint+1));
             return patrolPoints[indexOfPatrolPoint];
         }
     }
 
     private void SettingFullList(){
         for(int i=0; i<patrolPoints.Count; i++){
-            full.Add(0);
+            enemiesAtSamePatrolPoint.Add(0);
+        }
+    }
+
+    [HideInInspector] public void LeavingAPatrolPoint(Transform currentPatrolPoint){
+        for(int i = 0; i < patrolPoints.Count-1; i++){
+            if(patrolPoints[i] == currentPatrolPoint){
+                Debug.Log("Oduzet broj ljudi s " + patrolPoints[i]);
+                enemiesAtSamePatrolPoint[i]--;
+            }
         }
     }
 }

a5720c1cbff297ed324c1ee3d5424cbea9ac872b
JKadicps <kadic.jakov@gmail.com>
2024-01-27 12:36:56 +0100

    dash and jump

--- Assets/Scripts/PlayerScripts/KeyBinds.cs
+++ Assets/Scripts/PlayerScripts/KeyBinds.cs
@@ -9,6 +9,8 @@ public class KeyBinds : MonoBehaviour
     public KeyCode lvlRestartKey = KeyCode.R;
     public KeyCode sprintKey = KeyCode.LeftShift;
     public KeyCode attack = KeyCode.Mouse0;
+    public KeyCode jump = KeyCode.Space;
+    public KeyCode dash = KeyCode.E;
     
     private void Awake() {
         instance = this;
